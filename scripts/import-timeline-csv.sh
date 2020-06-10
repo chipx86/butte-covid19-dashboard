dbc07d0413c9645fca986149e3ba8400d77756d8
Christian Hammond <christian@beanbaginc.com>
2020-06-10 15:31:59 -0700

    Get rid of the old import-timeline-csv.sh script.
    This logic now exists in build-exports.py.

--- scripts/import-timeline-csv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-URL="https://docs.google.com/spreadsheets/d/e/2PACX-1vRwJpCeZj4tsxMXqrHFDjIis5Znv-nI0kQk9enEAJAbYzZUBHm7TELQe0wl2huOYEkdaWLyR8N9k_uq/pub?gid=856590862&single=true&output=csv"
-
-OUT_FILE="`dirname $0`/../htdocs/data/csv/timeline.csv"
-
-wget $URL -O $OUT_FILE
