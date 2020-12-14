0abf7c945db99e9137a9efd3db9a56ec876c4190
Christian Hammond <christian@beanbaginc.com>
2020-12-13 22:00:25 -0800

    Add additional per-hospital info to the hospital-cases datasets.
    The hospital-cases JSON and CSV datasets now include a total and
    per-hospital breakdown for:
    
    * Patient cases
    * Suspected patient cases
    * Patients in ICU
    * Suspected patients in ICU
    * Available ICU beds
    
    This makes use of the new Tableau parsing capabilities.

--- scripts/build-datasets.py
+++ scripts/build-datasets.py
@@ -1932,6 +1932,58 @@ def build_hospital_cases_json(session, response, out_filename, **kwargs):
             Expected data was missing or was in an unexpected format. Detailed
             information will be in the error message.
     """
+    def _get_col_data(label, total_pres_model_name, total_field_caption,
+                      total_result_key='total', include_date=False,
+                      expected_date=None):
+        data = tableau_loader.get_mapped_col_data({
+            total_pres_model_name: {
+                total_field_caption: {
+                    'data_type': 'integer',
+                    'result_key': 'total',
+                    'value_index': 0,
+                },
+            },
+            'Map Patients': {
+                'Hospital Name': {
+                    'data_type': 'cstring',
+                    'result_key': 'hospital_names',
+                    'normalize': lambda name: hospital_keys.get(name, name),
+                },
+                'AGG(Selector KPI)': {
+                    'data_type': 'real',
+                    'result_key': 'counts',
+                },
+            },
+            'Updated on': {
+                'max loaded timestamp': {
+                    'data_type': 'datetime',
+                    'result_key': 'date',
+                    'value_index': 0,
+                },
+            },
+        })
+
+        if expected_date is not None and data['date'] != expected_date:
+            raise ParseError('Date for hospitalizations data changed during '
+                             'data import. Try again.')
+
+        hospital_names = data['hospital_names']
+        counts = data['counts']
+
+        if len(hospital_names) != len(counts):
+            raise ParseError('Number of hospital names (%s) does not match '
+                             'number of case counts (%s) for %s.'
+                             % (len(hospital_names), len(counts), label))
+
+        col_data = dict({
+            total_result_key: data['total'],
+        }, **dict(zip(hospital_names, counts)))
+
+        if include_date:
+            col_data['date'] = data['date']
+
+        return col_data
+
     hospital_keys = {
         'Enloe Medical Center - Esplanade': 'enloe_hospital',
         'Oroville Hospital': 'oroville_hospital',
@@ -1951,50 +2003,73 @@ def build_hospital_cases_json(session, response, out_filename, **kwargs):
         }),
     })
 
-    data_dicts = tableau_loader.get_data_dicts()
+    result = {}
 
-    # Find the columns we care about.
-    data = tableau_loader.get_mapped_col_data({
-        'Map Patients': {
-            'Hospital Name': {
-                'data_type': 'cstring',
-                'result_key': 'hospital_names',
-                'normalize': lambda name: hospital_keys.get(name, name),
-            },
-            'AGG(Selector KPI)': {
-                'data_type': 'real',
-                'result_key': 'counts',
-            },
-        },
-        'Updated on': {
-            'max loaded timestamp': {
-                'data_type': 'datetime',
-                'result_key': 'date',
-            },
-        },
-    })
+    # Load the patients view.
+    patients_data = _get_col_data(
+        label='positive patients',
+        total_pres_model_name='Positive Patients',
+        total_field_caption='SUM(Hospitalized Covid Confirmed Patients)',
+        total_result_key='total_patients',
+        include_date=True)
 
-    date = datetime.strptime(data['date'][0], '%m/%d/%Y')
+    date_raw = patients_data['date']
+    date = datetime.strptime(date_raw, '%m/%d/%Y')
 
     if date > datetime.now():
         # This isn't today's date. Skip it.
         return False
 
-    hospital_names = data['hospital_names']
-    counts = data['counts']
+    result.update(patients_data)
+    result['date'] = date.strftime('%Y-%m-%d')
 
-    if len(hospital_names) != len(counts):
-        raise ParseError('Number of hospital names (%s) does not match '
-                         'number of case counts (%s).'
-                         % (len(hospital_names), len(counts)))
+    # Now load in information from the Suspected Patients view.
+    tableau_loader.set_parameter_value(
+        '[Parameters].[Select Measure (copy)_1581607928766861312]',
+        'Suspected Patients')
 
-    hospital_cases = {
-        hospital_name: count
-        for hospital_name, count in zip(hospital_names, counts)
-    }
-    hospital_cases['date'] = date.strftime('%Y-%m-%d')
+    result['suspected_patients'] = _get_col_data(
+        label='suspected patients',
+        total_pres_model_name='Suspected Patients',
+        total_field_caption='SUM(Hospitalized Suspected Covid Patients)',
+        expected_date=date_raw)
 
-    add_or_update_json_date_row(out_filename, hospital_cases)
+    # Now load in information from the ICU Available Beds view.
+    tableau_loader.set_parameter_value(
+        '[Parameters].[Select Measure (copy)_1581607928766861312]',
+        'ICU Available Beds')
+
+    result['icu_beds_available'] = _get_col_data(
+        label='available ICU beds',
+        total_pres_model_name='ICU Available Beds',
+        total_field_caption='SUM(Icu Available Beds)',
+        expected_date=date_raw)
+
+    # Now load in information from the ICU Positive Patients view.
+    tableau_loader.set_parameter_value(
+        '[Parameters].[Select Measure (copy)_1581607928766861312]',
+        'ICU Positive Patients')
+
+    result['icu_patients'] = _get_col_data(
+        label='ICU patients',
+        total_pres_model_name='ICU Positive Census',
+        total_field_caption='SUM(Icu Covid Confirmed Patients)',
+        expected_date=date_raw)
+
+    # Now load in information from the ICU Suspected Patients view.
+    tableau_loader.set_parameter_value(
+        '[Parameters].[Select Measure (copy)_1581607928766861312]',
+        'ICU Suspected Patients')
+
+    result['icu_suspected_patients'] = _get_col_data(
+        label='ICU suspected patients',
+        total_pres_model_name='ICU Suspected Census',
+        total_field_caption='SUM(Icu Suspected Covid Patients)',
+        expected_date=date_raw)
+
+    import pprint; pprint.pprint(result)
+
+    add_or_update_json_date_row(out_filename, result)
 
 
 def parse_csv(info, response, out_filename, **kwargs):
@@ -2568,9 +2643,25 @@ FEEDS = [
         'parser': convert_json_to_csv,
         'key_map': [
             ('Date', ('date',)),
-            ('Enloe Hospital', ('enloe_hospital',)),
-            ('Oroville Hospital', ('oroville_hospital',)),
-            ('Orchard Hospital', ('orchard_hospital',)),
+            ('Patients: Enloe Hospital', ('enloe_hospital',)),
+            ('Patients: Oroville Hospital', ('oroville_hospital',)),
+            ('Patients: Orchard Hospital', ('orchard_hospital',)),
+            ('Patients: Total', ('total_patients',)),
+        ] + [
+            ('%s: %s' % (_prefix, _hospital), (_type_key, _hospital_key))
+            for _prefix, _type_key in (('Suspected', 'suspected_patients'),
+                                       ('In ICU', 'icu_patients'),
+                                       ('Suspected in ICU',
+                                        'icu_suspected_patients'),
+                                       ('Available ICU Beds',
+                                        'icu_beds_available'))
+            for _hospital, _hospital_key in (('Enloe Hospital',
+                                              'enloe_hospital'),
+                                             ('Oroville Hospital',
+                                              'oroville_hospital'),
+                                             ('Orchard Hospital',
+                                              'orchard_hospital'),
+                                             ('Total', 'total'))
         ],
     },
     {
