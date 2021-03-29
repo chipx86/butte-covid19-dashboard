167917e2899e7aae7b5c699abf9f6e1e76e7ffbc
Christian Hammond <christian@beanbaginc.com>
2021-03-29 00:20:04 -0700

    Bring back the state-tiers datasets.
    These are being computed from
    https://github.com/datadesk/california-coronavirus-data/blob/master/cdph-reopening-tiers.csv
    and
    https://github.com/datadesk/california-coronavirus-data/blob/master/cdph-reopening-metrics.csv.
    
    The data goes back to August 31st. It's also been updated to include the
    Health Equity index.

--- scripts/bc19live/datasets/state_tiers.py
+++ scripts/bc19live/datasets/state_tiers.py
@@ -1,11 +1,15 @@
+import codecs
+import csv
 import json
 from datetime import datetime
 
 from bc19live.tableau import TableauLoader
-from bc19live.utils import add_or_update_json_date_row, convert_json_to_csv
+from bc19live.utils import (convert_json_to_csv,
+                            parse_real,
+                            safe_open_for_write)
 
 
-def build_dataset(session, response, out_filename, **kwargs):
+def build_dataset(session, responses, out_filename, **kwargs):
     """Parse the state tiers dashboard and build a JSON file.
 
     This parses the Tableau dashboard containing information on Butte County's
@@ -16,8 +20,8 @@ def build_dataset(session, response, out_filename, **kwargs):
         session (requests.Session):
             The HTTP request session, for cookie management.
 
-        response (requests.Respone):
-            The HTTP response containing the initial dashboard page.
+        responses (dict):
+            A mapping of keys to HTTP responses.
 
         out_filename (str):
             The filename for the JSON file to write.
@@ -34,69 +38,76 @@ def build_dataset(session, response, out_filename, **kwargs):
             Expected data was missing or was in an unexpected format. Detailed
             information will be in the error message.
     """
-    tableau_loader = TableauLoader(session=session,
-                                   owner='Planforreducingcovid-19',
-                                   sheet='Plan for reducing covid-19',
-                                   orig_response=response)
-    tableau_loader.bootstrap({
-        'showParams': json.dumps({
-            'unknownParams': 'County=Butte',
-        }),
-        'stickySessionKey': json.dumps({
-            'workbookId': 6582582,
-        }),
-    })
+    tiers = ['Widespread', 'Substantial', 'Moderate', 'Minimal']
 
-    data = tableau_loader.get_mapped_col_data({
-        'Map': {
-            'AGG(Avg Cases per Day per 100K)': {
-                'data_type': 'real',
-                'result_key': 'cases_per_100k',
-                'value_index': 0,
-                'normalize': lambda value: round(value, 2),
-            },
-            'AGG(Adj Avg Case Rate per Day per 100K)': {
-                'data_type': 'real',
-                'result_key': 'adjusted_cases_per_100k',
-                'value_index': 0,
-                'normalize': lambda value: round(value, 2),
-            },
-            'AGG(Test Positivity Rate)': {
-                'data_type': 'real',
-                'result_key': 'pos_rate',
-                'value_index': 0,
-                'normalize': lambda value: round(value, 5),
-            },
-            'Current tier': {
-                'data_type': 'integer',
-                'result_key': 'status',
-                'value_index': 0,
-            },
-            'Effective date': {
-                'data_type': 'cstring',
-                'result_key': 'effective_date',
-                'value_index': 0,
-                'normalize': lambda date_str: (
-                    datetime
-                    .strptime(date_str, '(as of %m/%d/%y)')
-                    .strftime('%Y-%m-%d')
-                ),
-            },
-        },
-    })
-    data['date'] = tableau_loader.get_last_update_date().strftime('%Y-%m-%d')
+    results = []
+    metrics_reader = csv.DictReader(
+        codecs.iterdecode(responses['metrics'].iter_lines(), 'utf-8'),
+        delimiter=',')
+    tiers_reader = csv.DictReader(
+        codecs.iterdecode(responses['tiers'].iter_lines(), 'utf-8'),
+        delimiter=',')
 
-    add_or_update_json_date_row(out_filename, data)
+    metric_rows = [
+        row
+        for row in metrics_reader
+        if row['county'] == 'Butte'
+    ]
+    metric_rows.reverse()
+
+    tiers_by_date = {
+        row['date']: int(row['tier'])
+        for row in tiers_reader
+        if row['county'] == 'Butte'
+    }
+
+    prev_tier = None
+
+    for metric_row in metric_rows:
+        date = metric_row['date']
+        prev_tier = tiers_by_date.get(date, prev_tier)
+
+        results.append({
+            'date': date,
+            'cases_per_100k': parse_real(
+                metric_row['percapita_case_rate'],
+                allow_blank=True),
+            'adjusted_cases_per_100k': parse_real(
+                metric_row['adjusted_case_rate'],
+                allow_blank=True),
+            'pos_rate': parse_real(
+                metric_row['positivity_rate'],
+                allow_blank=True),
+            'equity_index': parse_real(
+                metric_row['equity_index'],
+                allow_blank=True),
+            'status': tiers[prev_tier - 1],
+        })
+
+    with safe_open_for_write(out_filename) as fp:
+        json.dump(
+            {
+                'dates': results,
+            },
+            fp,
+            indent=2,
+            sort_keys=True)
 
 
 DATASETS = [
     {
         'filename': 'state-tiers.json',
         'format': 'json',
-        'url': (
-            'https://public.tableau.com/views/Planforreducingcovid-19/'
-            'planforreducingcovid-19/?%3AshowVizHome=no&County=Butte'
-        ),
+        'urls': {
+            'metrics': (
+                'https://raw.githubusercontent.com/datadesk/'
+                'california-coronavirus-data/master/cdph-reopening-metrics.csv'
+            ),
+            'tiers': (
+                'https://raw.githubusercontent.com/datadesk/'
+                'california-coronavirus-data/master/cdph-reopening-tiers.csv'
+            ),
+        },
         'parser': build_dataset,
     },
     {
@@ -114,6 +125,7 @@ DATASETS = [
             ('New Case Rate', ('cases_per_100k',)),
             ('Adjusted New Case Rate', ('adjusted_cases_per_100k',)),
             ('Positivity Rate', ('pos_rate',)),
+            ('Health Equity', ('equity_index',)),
         ],
     },
 ]
