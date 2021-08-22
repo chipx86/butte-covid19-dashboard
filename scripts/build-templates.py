7b56eac095592ca295cd4f49501a52928ed5e781
Christian Hammond <christian@beanbaginc.com>
2021-08-21 17:12:47 -0700

    Expand build-templates.py to make it easier to generate new files.
    The information on each template is now stored in a `TEMPLATES` list.
    The main template building operation will loop through this, building
    each one-by-one.

--- scripts/build-templates.py
+++ scripts/build-templates.py
@@ -5,6 +5,39 @@ import os
 from jinja2 import Environment, FileSystemLoader, Template
 
 
+TEMPLATES = [
+    {
+        'template': 'index.html.tmpl',
+        'out_filename': 'index.html',
+        'context': {
+            # Page header information
+            'preview_file': './images/preview.png',
+            'summary': (
+                'Detailed trends and information on the COVID-19 situation '
+                'in Butte County.'
+            ),
+            'title': 'Unofficial Butte County COVID-19 Dashboard',
+
+            # URLs
+            'county_dashboard_url':
+                'https://infogram.com/1pe66wmyjnmvkrhm66x9362kp3al60r57ex',
+        },
+    },
+]
+
+
+def build_template(env, dest_path, template, out_filename, context):
+    template = env.get_template(template)
+    html = template.render(context)
+
+    dest_file = os.path.join(dest_path, out_filename)
+
+    with open(dest_file, 'w') as fp:
+        fp.write(html)
+
+    print('Wrote %s' % dest_file)
+
+
 def main():
     dest_path = os.path.abspath(os.path.join(__file__, '..', '..', 'src'))
 
@@ -13,33 +46,8 @@ def main():
             os.path.abspath(os.path.join(__file__, '..', 'templates'))),
         keep_trailing_newline=True)
 
-    # Read in the template file.
-    template = env.get_template('index.html.tmpl')
-
-    # Build the data for the template
-    context = {
-        # Page header information
-        'preview_file': './images/preview.png',
-        'summary': (
-            'Detailed trends and information on the COVID-19 situation '
-            'in Butte County.'
-        ),
-        'title': 'Unofficial Butte County COVID-19 Dashboard',
-
-        # URLs
-        'county_dashboard_url':
-            'https://infogram.com/1pe66wmyjnmvkrhm66x9362kp3al60r57ex',
-    }
-
-    html = template.render(context)
-
-    # Write out the new HTML.
-    dest_file = os.path.join(dest_path, 'index.html')
-
-    with open(dest_file, 'w') as fp:
-        fp.write(html)
-
-    print('Wrote %s' % dest_file)
+    for template_info in TEMPLATES:
+        build_template(env, dest_path, **template_info)
 
 
 if __name__ == '__main__':
